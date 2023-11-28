6d6c211246e98c1b198bcbe3497510d37018f1c1
S4turn5 <pedromigueltiny30@gmail.com>
2023-11-28 00:51:32 +0000

    initial commit, BroCode lesson 1

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    // this is a comment
+    /*
+    multi-line comment
+    */
+    std::cout << "Hello World!" << '\n';
+    std::cout << "Name";
+
+    return 0;
+}
